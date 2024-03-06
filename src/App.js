2fdd51b7297f4db41584f5febdf21c5dcf56897e
Tom Creuse <tom.creuse@gmail.com>
2024-03-06 08:48:13 +0100

    Ajout du redirect vers Erreur sur App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  BrowserRouter as Router,
+  redirect,
+} from "react-router-dom";
 import Accueil from "./Pages/Accueil";
 import APropos from "./Pages/Apropos";
 import Erreur from "./Pages/Erreur";
@@ -10,7 +15,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Accueil />} />
         <Route path="/Apropos" element={<APropos />} />
-        <Route element={<Erreur />} />
+        <Route path="/Erreur" element={<Erreur />} />
+        <Route element={<redirect to="/Erreur" />} />
       </Routes>
     </Router>
   );
