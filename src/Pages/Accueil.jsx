4591e1d54a21d35d6f475f888751adf0c13d59d6
Tom Creuse <tom.creuse@gmail.com>
2024-03-08 15:48:17 +0100

    banner intégrée (css à faire)

--- src/Pages/Accueil.jsx
+++ src/Pages/Accueil.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Layout from "../components/layout/layout";
+import Banner from "../components/banner/banner";
+import homeBanner from "../ASSETS/imgbanner.jpg";
 
 export default function Accueil() {
   return (
     <Layout>
-      <h1>Page d'accueil</h1>
-      {/* Autres éléments */}
+      <Banner imageSrc={homeBanner} imageAlt="Falaise au dessus de l'océan" text="Chez vous, partout et ailleurs" />
+            {/* Autres éléments */}
     </Layout>
   );
 };
--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -1,6 +1,6 @@
 import "./banner.scss"
 
-function banner ({imageSrc, imageAlt, text=""}) {
+function Banner ({imageSrc, imageAlt, text=""}) {
     return (<header className="banner-container">
     <img className="banner-img" src={imageSrc} alt={imageAlt} />
     <blockquote className="banner-text">{text}</blockquote>
