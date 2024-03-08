9c028d1ed1b58a746c0e69bfeb49b736893d11bb
Tom Creuse <tom.creuse@gmail.com>
2024-03-08 15:36:36 +0100

    banner pas encore testée

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -1,5 +1,10 @@
 import "./banner.scss"
 
-function banner ({}) {
-    return 
+function banner ({imageSrc, imageAlt, text=""}) {
+    return (<header className="banner-container">
+    <img className="banner-img" src={imageSrc} alt={imageAlt} />
+    <blockquote className="banner-text">{text}</blockquote>
+  </header>)
 }
+
+export default Banner;
