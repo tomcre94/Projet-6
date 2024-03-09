cce07f9f0a66dd404552b2937c4c424f512966de
Tom Creuse <tom.creuse@gmail.com>
2024-03-09 06:04:14 +0100

    insertion des cards de logement fonctionne (scss à faire)

---
+++ src/JsonComponent.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import Card from "./components/card/card";
+
+export default function JsonComponent() {
+  const [jsonData, setJsonData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("logements.json");      
+          const parsedData = await response.json();
+          setJsonData(parsedData);
+          console.log(parsedData);
+       
+      } catch (error) {
+        console.error(
+          "Une erreur s'est produite lors de la récupération des données JSON :",
+          error
+        );
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <figure className='card-container'>
+      {jsonData.length > 0 ? (
+        jsonData.map((accommodation) => (
+          <Card 
+            key={accommodation.id}
+            id={accommodation.id}
+            title={accommodation.title}
+            cover={accommodation.cover}
+          /> 
+        ))
+      ) : (
+        <p>Aucune donnée disponible pour le moment.</p>
+      )}
+    </figure>
+  );
+}
--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,17 +1,17 @@
 import "./card.scss";
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 
 function Card ({id, title, cover}) {
 
     return (
-        <Link to={`/Accomodation/${id}`} className='card'>
+        //<Link to={`/Accomodation/${id}`} className='card'>
       <figure className='card-figure'>
         <img src={cover} alt={title} className='card-img'></img>
         <figcaption className='card-linear-gradient'>
           <p className='card-figcaption'>{title}</p>
         </figcaption>
       </figure>
-    </Link> 
+    //</Link> 
 
     )
 }
