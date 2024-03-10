1fc98e566191776abac6d60ba594a3b734bea0dc
Tom Creuse <tom.creuse@gmail.com>
2024-03-10 21:09:04 +0100

    collapse fonctionnelle (remplir description + CSS)

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../../components/layout/layout";
 import "./About.scss";
 import Banner from "../../components/banner/banner";
 import Imgabout from "../../ASSETS/imgapropos.jpg"
 
+const Module = ({ title, description }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div>
+      <div onClick={() => setIsOpen(!isOpen)}>
+        {title} <span>{isOpen ? '↓' : '→'}</span>
+      </div>
+      {isOpen && <p>{description}</p>}
+    </div>
+  );
+};
+
+const App = () => (
+  <>
+    <Module title="Fiabilité" description="Description de la fiabilité." />
+    <Module title="Respect" description="Description du respect." />
+    <Module title="Service" description="Description du service." />
+    <Module title="Sécurité" description="Description de la sécurité." />
+  </>
+);
+
 export default function About() {
   return (
     <Layout>
       <Banner imageSrc={Imgabout} imageAlt="Falaise au dessus de l'océan" text=""/>
-
-      <h1>À propos</h1>
-      {/* Autres éléments */}
+      le main contient 4 div composées d'un rectangle rouge et d'un texte écrit en blanc (Fiabilité, Respect, Service, Sécurité,)
+      <App />
     </Layout>
   );
 };
