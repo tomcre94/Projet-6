285a5b3913c32c6b668b3236230c9cea1d888bd8
Tom Creuse <tom.creuse@gmail.com>
2024-03-10 22:32:01 +0100

    about fonctionnel (ajouter google font pour la police et les icones)

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -3,34 +3,39 @@ import Layout from "../../components/layout/layout";
 import "./About.scss";
 import Banner from "../../components/banner/banner";
 import Imgabout from "../../ASSETS/imgapropos.jpg"
+import VectorUp  from "../../ASSETS/Vectorvectorup.jpg"
+import VectorDown  from "../../ASSETS/Vectorvectordown.jpg"
 
 const Module = ({ title, description }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div>
-      <div onClick={() => setIsOpen(!isOpen)}>
-        {title} <span>{isOpen ? '↓' : '→'}</span>
+    <div className="collapse">
+      <div onClick={() => setIsOpen(!isOpen)} className={"title-module"}>
+       <span className="title">{title}</span> <span className="arrow">{isOpen ? <span class="material-icons">
+expand_more
+</span> : <span class="material-icons">
+expand_less
+</span>}</span>
       </div>
-      {isOpen && <p>{description}</p>}
+      {isOpen && <p className={"description-module"}>{description}</p>}
     </div>
   );
 };
 
 const App = () => (
-  <>
-    <Module title="Fiabilité" description="Description de la fiabilité." />
-    <Module title="Respect" description="Description du respect." />
-    <Module title="Service" description="Description du service." />
-    <Module title="Sécurité" description="Description de la sécurité." />
-  </>
+  <div>
+    <Module title="Fiabilité" description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes." />
+    <Module title="Respect" description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
+    <Module title="Service" description="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question." />
+    <Module title="Sécurité" description="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
+  </div>
 );
 
 export default function About() {
   return (
     <Layout>
       <Banner imageSrc={Imgabout} imageAlt="Falaise au dessus de l'océan" text=""/>
-      le main contient 4 div composées d'un rectangle rouge et d'un texte écrit en blanc (Fiabilité, Respect, Service, Sécurité,)
       <App />
     </Layout>
   );
