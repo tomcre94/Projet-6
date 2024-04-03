5933df831d66b5de2f806d225b9aaf103e0bf6de
Tom Creuse <tom.creuse@gmail.com>
2024-04-03 16:29:53 +0200

    transition added on collapse

--- src/components/module/module.jsx
+++ src/components/module/module.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./module.scss";
-import { SlArrowDown, SlArrowUp } from "react-icons/sl";
+import { SlArrowDown } from "react-icons/sl";
 
 const Module = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -8,7 +8,7 @@ const Module = ({ children }) => {
   const toggleOpen = () => {
     setIsOpen(!isOpen);
     const firstChild = React.Children.toArray(children)[0];
-    if (!isOpen && firstChild.type === "h2" && firstChild.props.children === "Equipements") {
+    if (!isOpen && firstChild.type === "h2") {
       document.querySelector("main").classList.add("description-open");
     } else {
       document.querySelector("main").classList.remove("description-open");
@@ -20,16 +20,14 @@ const Module = ({ children }) => {
     <div className={`collapse ${isOpen ? "open" : ""}`}>
       <div onClick={toggleOpen} className={"title-module"}>
         <span className="title">{React.Children.toArray(children)[0]}</span>
-        <span className="arrow">
-          {isOpen ? <SlArrowDown /> : <SlArrowUp />}
+        <span className={`arrow ${isOpen ? "open" : "closed"}`}>
+          <SlArrowDown />
         </span>
       </div>
-      {isOpen && (
-        <div className="description-module">
-        {React.Children.toArray(children).slice(1)}
-        </div>
-      )}
+    <div className={`description-module ${isOpen ? "open" : ""}`}>
+     {React.Children.toArray(children).slice(1)}
     </div>
+  </div>
   );
 };
 
